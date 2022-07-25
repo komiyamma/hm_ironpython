c4df77e940328c339d061ef65f6b71a2972af1b5
komiyamma <komiyama0629@gmail.com>
2022-07-25 09:42:15 +0900

    result が 0 の際の例外はなし

--- hmPy.src/hmPyStaticLib/hmPyHidemaruMacro.cs
+++ hmPy.src/hmPyStaticLib/hmPyHidemaruMacro.cs
@@ -503,15 +503,7 @@ public sealed partial class hmPyDynamicLib
 
                 if (result.Error == null)
                 {
-                    if (macro_result <= 0)
-                    {
-                        result.Error = new InvalidOperationException("HidemaruMacroResultZeroException");
-                        result.Result = macro_result;
-                    }
-                    else
-                    {
-                        result.Result = macro_result;
-                    }
+                    result.Result = macro_result;
                 }
                 tmpVar = null; // クリア
 
